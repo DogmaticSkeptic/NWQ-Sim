1fc97cc888762dfd2772ebc38627a73410d19a94
DogmaticSkeptic <ahoyt2014@gmail.com>
2025-06-08 13:46:41 -0700

    Added support for checking if TAMM or ITENSOR is enabled

--- include/backendManager.hpp
+++ include/backendManager.hpp
@@ -6,11 +6,18 @@
 #include "svsim/sv_cpu.hpp"
 #include "dmsim/dm_cpu.hpp"
 
+#ifdef ITENSOR_ENABLED
 #include "tnsim/tn_itensor.hpp"
-#ifdef Error
-#  undef Error
 #endif
+
+// Both itensor and TAMM have Error (may be a better way to do this but this works)
+#ifdef Error
+#undef Error
+#endif
+
+#ifdef TAMM_ENABLED
 #include "tnsim/tn_tamm.hpp"
+#endif
 
 #ifdef OMP_ENABLED
 #include "svsim/sv_omp.hpp"
