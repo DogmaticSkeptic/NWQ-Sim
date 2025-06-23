f3d2b2b3a08ea9a7c6ea0b6dbb7764dffc1ec1d9
DogmaticSkeptic <ahoyt2014@gmail.com>
2025-06-23 16:25:59 -0700

    Fixed littlebig

--- include/tnsim/tn_tamm.hpp
+++ include/tnsim/tn_tamm.hpp
@@ -512,7 +512,7 @@ namespace NWQSim
             tamm::Tensor<Cplx> M2({bond_tis[q0], phys_tis[q0], phys_tis[q1], bond_tis[q1 + 1]});
             M2.set_dense();
             M2.allocate(&ec);
-            std::cout<<"Bond Dimension:"<<bond_tis[q0]<<","<<bond_tis[q1 + 1];
+            std::cout<<"Bond Dimension:"<<bond_dims[q0]<<","<<bond_dims[q1 + 1];
             auto t0 = std::chrono::high_resolution_clock::now();
 
             sch(M2("l","p0p","p1p","r") =
