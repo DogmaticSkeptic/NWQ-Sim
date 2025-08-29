d01d99ff95569a3d149525da37832f8dba8c6556
DogmaticSkeptic <ahoyt2014@gmail.com>
2025-08-28 20:51:25 -0700

    Fast Push for Uploading Code to Remote

--- include/tnsim/tn_tamm.hpp
+++ include/tnsim/tn_tamm.hpp
@@ -541,8 +541,6 @@ namespace NWQSim
             }
             if (rank == 0) std::cout << "simulation_kernel: Diagnostic check passed. All layers are conflict-free." << std::endl;
         
-            return;
-
             // ************************************************************************
             // EXECUTION: Execute the correctly formed layers.
             // ************************************************************************
