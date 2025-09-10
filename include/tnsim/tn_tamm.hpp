2e8c89dafcf979cfb11ef1fdf35931ce9e3549db
DogmaticSkeptic <ahoyt2014@gmail.com>
2025-09-10 15:43:58 -0700

    Fast Push for Uploading Code to Remote

--- include/tnsim/tn_tamm.hpp
+++ include/tnsim/tn_tamm.hpp
@@ -1325,7 +1325,7 @@ namespace NWQSim
             
             // 6a. Populate the new left tensor data (from Umat)
             Ti_new_data.resize(Dl * phys_dim * chi);
-            int c = 0;
+            c = 0;
             for (size_t l = 0; l < Dl; ++l) {
                 for (size_t p0 = 0; p0 < phys_dim; ++p0) {
                     for (size_t b = 0; b < chi; ++b, ++c) {
